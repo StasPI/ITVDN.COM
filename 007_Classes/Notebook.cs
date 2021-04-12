983355353528aa57f0987a6ebf4c96eeffc53ab2
stanislav.parfilov <stanislav.parfilov@ics-it.ru>
2021-04-12 19:47:46 +0300

    test

---
+++ 007_Classes/Notebook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Classes
+{
+   public struct Notebook
+   {
+      string model;
+      string manufactur;
+      int price;
+
+      public Notebook(string model, string manufacturer, int price)
+      {
+         this.model = model;
+         this.manufactur = manufacturer;
+         this.price = price;
+      }
+
+      public Notebook(string model): this(model, "", 0)
+      {
+      }
+
+      //public Notebook(string manufacturer): this("", manufacturer, 0)
+      //{
+      //}
+
+      public Notebook(int price): this("", "", price)
+      {
+      }
+
+      public void Show()
+      {
+         Console.WriteLine($"model: {model}, manufacturer: {manufactur}, price: {price}.");
+      }
+   }
+}
---
+++ 007_Classes/Train.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Classes
+{
+   public struct Train
+   {
+      string destinationName;
+      int trainNumber;
+      DateTime departureTime;
+
+      public Train (string destinationName, int trainNumber, DateTime departureTime)
+      {
+         this.destinationName = destinationName;
+         this.trainNumber = trainNumber;
+         this.departureTime = departureTime;
+      }
+   }
+}
