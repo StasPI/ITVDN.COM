5a96f4562237be6b53155955df7f5b6d6df1bd2d
stanislav.parfilov <stanislav.parfilov@ics-it.ru>
2021-04-13 11:08:40 +0300

    new

--- 007_Classes/Program.cs
+++ 007_Classes/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _007_Classes
 {
@@ -23,15 +25,75 @@ namespace _007_Classes
          ////-ввод с клавиатуры данных в массив, состоящий из восьми элементов типа Train(записи должны быть упорядочены по номерам поездов);
          ////-вывод на экран информации о поезде, номер которого введен с клавиатуры(если таких поездов нет, вывести соответствующее сообщение).
 
-         List<Train> t1 = new List<Train>();
-         t1.Add(new Train("a", 1, DateTime.Now));
- 
+         //Train[] tr1 = new Train[2];
+         //string destinationName;
+         //int trainNumber;
+         //int sw = 0;
+
+         //// Принять с консоли
+         //for (int i = 0; i < tr1.Length; ++i)
+         //{
+         //   Console.WriteLine($"Введи пункт назначения для {i + 1} из {tr1.Length}");
+         //   destinationName = Console.ReadLine();
+         //   Console.WriteLine("Введи номер поезда");
+         //   trainNumber = Convert.ToInt32(Console.ReadLine());
+
+         //   tr1[i] = new Train(destinationName, trainNumber, DateTime.Now);
+         //}
+         //// Сортировка
+         //List<Train> tr2 = tr1.OrderBy(si => si.trainNumber).ToList();
+
+         //// Вывод информации по нужному поезду
+         //Console.WriteLine("Введите номер поезда что бы получить информацию по нему");
+         //trainNumber = Convert.ToInt32(Console.ReadLine());
+
+         //for (int i = 0; i < tr1.Length; ++i)
+         //{
+         //   if (tr1[i].trainNumber == trainNumber)
+         //   {
+         //      Console.WriteLine($"{tr1[i].trainNumber}, {tr1[i].destinationName}, {tr1[i].departureTime}");
+         //      sw = 1;
+         //   }
+         //}
+         //if (sw != 1)
+         //{
+         //   Console.WriteLine($"Херня какая то");
+         //}
+
          ////Задание 3
          ////Создайте класс MyClass и структуру MyStruct, которые содержат в себе поля public string change.
          ////В классе Program  создайте два  метода:  
          ////- static void ClassTaker(MyClass myClass), который присваивает полю change экземпляра myClass значение «изменено». 
          ////- static void StruktTaker(MyStruct myStruct), который присваивает полю change экземпляра myStruct значение «изменено». 
-         ////Продемонстрируйте разницу в использовании классов и структур, создав в методе Main() экземпляры структуры и класса.Измените, значения полей экземпляров на «не изменено», а затем вызовите методы ClassTaker и StruktTaker.Выведите на экран значения полей экземпляров. Проанализируйте полученные результаты.
+         ////Продемонстрируйте разницу в использовании классов и структур, создав в методе Main() экземпляры структуры и класса.Измените, 
+         ////значения полей экземпляров на «не изменено», а затем вызовите методы ClassTaker и StruktTaker.Выведите на экран значения полей экземпляров. 
+         ////Проанализируйте полученные результаты.
+
+         MyClass myClass = new MyClass();
+         MyStruct myStruct = new MyStruct();
+
+         myClass.change = "MyClass не изменено";
+         myStruct.change = "MyStruct не изменено";
+
+         Console.WriteLine(myClass.change);
+         Console.WriteLine(myStruct.change);
+
+         ClassTaker(myClass);
+         StruktTaker(myStruct);
+
+         Console.WriteLine(myClass.change);
+         Console.WriteLine(myStruct.change);
+
+         Console.ReadKey();
       }
-}
+
+      static void ClassTaker(MyClass myClass)
+      {
+         myClass.change = "MyClass изменено";
+      }
+      static void StruktTaker(MyStruct myStruct)
+      {
+         myStruct.change = "MyStruct изменено";
+      }
+   }
 }
--- 007_Classes/Train.cs
+++ 007_Classes/Train.cs
@@ -8,15 +8,20 @@ namespace _007_Classes
 {
    public struct Train
    {
-      string destinationName;
-      int trainNumber;
-      DateTime departureTime;
+      public string destinationName;
+      public int trainNumber;
+      public DateTime departureTime;
 
       public Train (string destinationName, int trainNumber, DateTime departureTime)
       {
          this.destinationName = destinationName;
          this.trainNumber = trainNumber;
          this.departureTime = departureTime;
+      }
+
+      public void Show()
+      {
+         Console.WriteLine($"destinationName: {destinationName}, trainNumber: {trainNumber}, departureTime: {departureTime}.");
       }
    }
 }
